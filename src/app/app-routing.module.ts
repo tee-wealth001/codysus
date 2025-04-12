c60e529587f9903be23ed50a8515ed4649242b60
stephen <stephenola21@gmail.com>
2025-04-12 16:40:12 +0100

    setup home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent }, // Define home explicitly
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect root to home
+  { path: '**', redirectTo: '/home' } // Handle undefined routes
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
