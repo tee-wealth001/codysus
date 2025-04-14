5472cce3136cdbc50cea29fd0caf53ab4c0e00c4
stephen <stephenola21@gmail.com>
2025-04-15 00:16:02 +0100

    integrated calendly

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+declare var Calendly: any;
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  showCalendly = false;
+
+  loadCalendly() {
+    this.showCalendly = true;
+
+    setTimeout(() => {
+      Calendly.initInlineWidget({
+        url: 'https://calendly.com/stephentoriola',
+        parentElement: document.getElementById('calendly-container'),
+        prefill: {},
+        utm: {}
+      });
+    }, 0); // Let Angular render the div before initializing
+  }
+
+  cancelCalendly() {
+    const container = document.getElementById('calendly-container');
+    if (container) container.innerHTML = '';
+    this.showCalendly = false;
+  }
 
 }
