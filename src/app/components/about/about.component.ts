1f3608b9056b41886f54a2c160aec8f08f0877af
stephen <stephenola21@gmail.com>
2025-04-15 20:10:24 +0100

    made techstack dynamic

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  techStackImages = [
+    'c++.png',
+    'python.png',
+    'javascript.png',
+    'angular.png',
+    'java.png',
+    'html.png',
+    'css.png',
+    'typescript.png',
+    'bootstrap.png',
+    'nodejs.png',
+    'mysql.png',
+    'postgresql.png',
+    'git.png',
+    'github.png',
+    'gcp.png',
+    'firebase.png',
+    'netlify.png',
+    'vscode.png',
+    'intellij.png',
+    'postman.png',
+    'php.png',
+    'react.png',
+    'scratchjr.png',
+    'csharp.png',
+    'machinelearning.png',
+    'tensorflow.png',
+    'ai.png',
+    'sklearn.png',
+    'r.png',
+    'ionic.png',
+    'flutter.png',
+    'datascience.png',
+  ];
+  
+
 }
