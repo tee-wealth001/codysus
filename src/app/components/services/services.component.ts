8c2f1b73a0bf849963cd73d416e986a9fb63ba41
stephen <stephenola21@gmail.com>
2025-04-16 21:49:35 +0100

    added scheduling button

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+declare var Calendly: any;
+
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+
+  showCalendly = false;
+
+  loadCalendly() {
+    this.showCalendly = true;
+
+    setTimeout(() => {
+      Calendly.initInlineWidget({
+        url: 'https://calendly.com/stephentoriola',
+        parentElement: document.getElementById('calendly-container'),
+        prefill: {},
+        utm: {}
+      });
+    }, 0); // Let Angular render the div before initializing
+  }
+
+  cancelCalendly() {
+    const container = document.getElementById('calendly-container');
+    if (container) container.innerHTML = '';
+    this.showCalendly = false;
+  }
 
 }
