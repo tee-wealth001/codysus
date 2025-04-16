e6cbb177055ee136908ad928a1f90da01e6f33c2
stephen <stephenola21@gmail.com>
2025-04-16 22:33:29 +0100

    code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -26,7 +26,7 @@ import { ServicesComponent } from './components/services/services.component';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [provideHttpClient(withInterceptorsFromDi())],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
